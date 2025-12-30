9d77b2307ebccb798234799c15250ba9918907ca
wangshan <717208731@qq.com>
2025-12-30 13:32:29 +0800

    修改页面显示的内容

--- app/reown/page.tsx
+++ app/reown/page.tsx
@@ -1,31 +1,6 @@
-// import { cookieStorage, createStorage, http } from '@wagmi/core'
-import { ConnectButton } from "./components/ConnectButton";
-import { InfoList } from "./components/InfoList";
-import { ActionButtonList } from "./components/ActionButtonList";
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-
-  return (
-    <div className="space-y-8">
-      <h1 className="text-2xl font-bold">AppKit Wagmi Next.js App Router Example</h1>
-
-      <ConnectButton />
-      
-      <div className="border-b border-gray-300 my-4"></div>
-      
-      <ActionButtonList />
-      
-      <div className="border-b border-gray-300 my-4"></div>
-      
-      <div className="advice">
-        <p>
-          This projectId only works on localhost. <br/>Go to <a href="https://dashboard.reown.com" target="_blank" className="link-button" rel="Reown Dashboard">Reown Dashboard</a> to get your own.
-        </p>
-      </div>
-      
-      <div className="border-b border-gray-300 my-4"></div>
-      
-      <InfoList />
-    </div>
-  );
+// 重定向到wallet页面，实现点击reown导航时默认显示wallet内容
+export default function ReownDefaultPage() {
+  redirect('/reown/wallet');
 }
--- app/reown/wallet/page.tsx
+++ app/reown/wallet/page.tsx
@@ -1,7 +1,31 @@
+// import { cookieStorage, createStorage, http } from '@wagmi/core'
+import { ConnectButton } from "../components/ConnectButton";
+import { InfoList } from "../components/InfoList";
+import { ActionButtonList } from "../components/ActionButtonList";
+
 export default function WalletPage() {
+
   return (
-    <div>
-      <h1>reown Wallet</h1>
+    <div className="space-y-8">
+      <h1 className="text-2xl font-bold">AppKit Wagmi Next.js App Router Example</h1>
+
+      <ConnectButton />
+      
+      <div className="border-b border-gray-300 my-4"></div>
+      
+      <ActionButtonList />
+      
+      <div className="border-b border-gray-300 my-4"></div>
+      
+      <div className="advice">
+        <p>
+          This projectId only works on localhost. <br/>Go to <a href="https://dashboard.reown.com" target="_blank" className="link-button" rel="Reown Dashboard">Reown Dashboard</a> to get your own.
+        </p>
+      </div>
+      
+      <div className="border-b border-gray-300 my-4"></div>
+      
+      <InfoList />
     </div>
-  )
+  );
 }
